9cd4952c869e4f1c5a71c6ddb6eb907099c4fab5
Kimia Darden <kimiadarden@gmail.com>
2020-02-26 21:41:07 -0500

    adding the writefile

--- Develop/app.js
+++ Develop/app.js
@@ -5,3 +5,24 @@ const fs= require("fs");
 const inquirer= require("inquirer");
 const path= require("path");
 
+async function mainStarting(){
+
+
+
+
+
+
+
+    fs.writeFile("templates/finalPage.html", teamHTML, function(err) {
+
+        if (err) {
+          return err;
+        }
+      
+        console.log("The final page was made succesfully!");
+      
+      });  
+}
+
+
+mainStarting()

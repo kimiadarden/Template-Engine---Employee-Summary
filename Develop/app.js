c34eb8e84577c47bc77ebca51cff8cb2925be43d
Kimia Darden <kimiadarden@gmail.com>
2020-02-26 21:57:46 -0500

    adding promt

--- Develop/app.js
+++ Develop/app.js
@@ -25,10 +25,42 @@ async function mainStarting(){
         console.log("Your team has no members");
         return;
     }
+    for(i = 1; i < numberOfteam; i++){
+
+        let name;
+        let id;
+        let title;
+        let email;
+
+        await inquirer.prompt([ 
+            {
+                type: "input",
+                message: `What is the employee (${i})'s id?`,
+                name: "id"
+            },
+            {
+                type: "input",
+                message: `What is employee (${i})'s name?`,
+                name: "name"
+            },
+            {
+                type: "input",
+                message: `What is the employee (${i})'s Email?`,
+                name: "email"
+            },
+            {
+                type: "list",
+                message: `what the employee (${i})'s title?`,
+                name: "title",
+                choices: ["Engineer", "Intern", "Manager"]
+            }
+        ])
+
+
+
+    const mainHTML = fs.readFileSync("templates/main.html");
     
-
-
-
+    htmlForTeam = eval('`'+ mainHTML +'`');
 
     fs.writeFile("templates/finalPage.html", teamHTML, function(err) {
 
